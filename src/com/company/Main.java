14ca18c91a40a9068fe902711fc548f418c7ba1f
Alejandro <alemg77@yahoo.com.ar>
2020-03-14 18:27:49 -0300

    Tres

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Hashtable;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo cruel");
-	// write your code here
+
+        Hashtable<Integer,String> tablaQuiniela = new Hashtable<>();
+
+        tablaQuiniela.put(0,"Huevos");
+        tablaQuiniela.put(1,"Agua");
+        tablaQuiniela.put(2,"Escopeta");
+        tablaQuiniela.put(3,"Caballo");
+        tablaQuiniela.put(4,"Dentista");
+        tablaQuiniela.put(5,"Llamas");
+
+
+        System.out.println(tablaQuiniela.get(3));
+
+        for (Integer integer : tablaQuiniela.keySet()) {
+            System.out.println(tablaQuiniela.get(integer));
+        }
     }
+
 }
